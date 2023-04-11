619830bda8589e596611a8c11abe5aa8571131c9
Hossein <77438070+Hossein-Haghshenas@users.noreply.github.com>
2023-04-11 19:11:35 +0430

    Fix something

--- client/src/pages/Customizer.jsx
+++ client/src/pages/Customizer.jsx
@@ -7,16 +7,12 @@ import { download } from "../assets";
 import { downloadCanvasToImage, reader } from "../config/helpers";
 import { EditorTabs, FilterTabs, DecalTypes } from "../config/constants";
 import { fadeAnimation, slideAnimation } from "../config/motion";
-import { AIPicker, ColorPicker, CustomButton, FilePicker, Tab } from "../components";
+import { ColorPicker, CustomButton, FilePicker, Tab } from "../components";
 
 const Customizer = () => {
     const snap = useSnapshot(state)
 
     const [file, setFile] = useState('');
-
-    const [prompt, setPrompt] = useState('');
-
-    const [generatingImg, setGeneratingImg] = useState(false);
 
     const [activeEditorTab, setActiveEditorTab] = useState("");
     const [activeFilterTab, setActiveFilterTab] = useState({
@@ -30,8 +26,8 @@ const Customizer = () => {
                 return <ColorPicker />
             case "filepicker":
                 return <FilePicker file={file} setFile={setFile} readFile={readFile} />
-            case "aipicker":
-                return <AIPicker />
+            // case "aipicker":
+            //     return <AIPicker />
             default:
                 return null
         }
